c684921e7fd8e11700c85a9b05eed5f442b131ed
NoNiQu <marcos.workstation@gmail.com>
2024-12-14 14:43:41 +0100

    build3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,17 @@
 import "../style/main.scss";
-import * as icons from "./icons.js";
-import * as idioma from "./idioma.js";
+// import * as icons from "./icons.js";
+// import * as idioma from "./idioma.js";
 
-const progDiv = document.querySelector("#prog");
-const desDiv = document.querySelector("#des");
+// const progDiv = document.querySelector("#prog");
+// const desDiv = document.querySelector("#des");
 
-const iconsDes = [];
-const iconsProg = [];
+// const iconsDes = [];
+// const iconsProg = [];
 
-let positionsProg = [];
-let positionsDes = [];
+// let positionsProg = [];
+// let positionsDes = [];
 
-icons.addIcons(progDiv, "icons prog", iconsProg, positionsProg);
-icons.addIcons(desDiv, "icons des", iconsDes, positionsDes);
+// icons.addIcons(progDiv, "icons prog", iconsProg, positionsProg);
+// icons.addIcons(desDiv, "icons des", iconsDes, positionsDes);
 
-idioma.changeLanguage("es");
+// idioma.changeLanguage("es");
